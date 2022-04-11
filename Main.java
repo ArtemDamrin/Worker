efebb8928188d9360cddf9d44fb850c13961f5fe
ArtemDamrin <damrin.a@yandex.ru>
2022-04-11 20:37:36 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener listener1 = System.out::println;
+        // Worker worker = new Worker(listener);
+        Worker worker1 = new Worker(listener1);
+
+        worker1.start();
+    }
+}
---
+++ Worker.java
@@ -0,0 +1,26 @@
+import java.util.function.Predicate;
+
+public class Worker {
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback) {
+        this.callback = callback;
+    }
+
+    public Worker(OnTaskErrorListener errorCallback) {
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (task33.test(i)) {
+                errorCallback.onError("Task " + i + " is done");
+            } else {
+                errorCallback.onError("Error! Task " + i + " is not solved yet.");
+            }
+        }
+    }
+    Predicate<Integer> task33 = x -> x != 33;
+}
